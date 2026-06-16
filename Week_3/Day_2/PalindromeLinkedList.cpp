2a26133a762f5bbb0df2db4f961a27dc71876397
Aryan006-code <aryansri1866@gmail.com>
2026-06-16 18:33:03 +0530

    Create PalindromeLinkedList.cpp

---
+++ Week_3/Day_2/PalindromeLinkedList.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+
+    ListNode* reverse(ListNode* head){
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+
+        while(curr){
+            ListNode* next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    bool isPalindrome(ListNode* head) {
+
+        if(!head || !head->next)
+            return true;
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast->next && fast->next->next){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        ListNode* second = reverse(slow->next);
+
+        ListNode* first = head;
+
+        while(second){
+            if(first->val != second->val)
+                return false;
+
+            first = first->next;
+            second = second->next;
+        }
+        return true;
+    }
+};
