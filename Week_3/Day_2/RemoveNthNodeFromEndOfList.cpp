266391bd2407b524d99efcf625c7926e81589e48
Aryan006-code <aryansri1866@gmail.com>
2026-06-16 18:32:34 +0530

    Create RemoveNthNodeFromEndOfList.cpp

---
+++ Week_3/Day_2/RemoveNthNodeFromEndOfList.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+
+        ListNode dummy(0);
+        dummy.next = head;
+
+        ListNode* fast = &dummy;
+        ListNode* slow = &dummy;
+
+        for(int i = 0; i <= n; i++){
+            fast = fast->next;
+        }
+
+        while(fast){
+            fast = fast->next;
+            slow = slow->next;
+        }
+
+        ListNode* del = slow->next;
+        slow->next = slow->next->next;
+        delete del;
+        
+        return dummy.next;
+    }
+};
