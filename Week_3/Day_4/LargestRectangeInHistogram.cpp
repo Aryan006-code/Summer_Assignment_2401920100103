ff9bb99ff5847210e1e7cff2453101b8d71ef90a
Aryan006-code <aryansri1866@gmail.com>
2026-06-18 16:36:18 +0530

    Create LargestRectangeInHistogram.cpp

---
+++ Week_3/Day_4/LargestRectangeInHistogram.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
+        int maxArea = 0;
+        stack<int> st;
+
+        for(int i = 0; i < n; i++){
+            while(!st.empty() && heights[st.top()] > heights[i]){
+                int element = st.top();
+                st.pop();
+                int nse = i;
+                int pse = st.empty() ? -1 : st.top();
+                maxArea = max(heights[element] * (nse-pse-1),maxArea);
+            }
+            st.push(i);
+
+        }
+        while(!st.empty()){
+                int nse = n;
+                int element = st.top();
+                st.pop();
+                int pse = st.empty() ? -1 : st.top();
+                maxArea = max(heights[element] * (nse-pse-1),maxArea);
+            }
+        return maxArea;
+    }
+};
