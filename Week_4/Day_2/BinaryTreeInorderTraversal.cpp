300624ac615763efec6818e22f3a97ba87a09fe8
Aryan006-code <aryansri1866@gmail.com>
2026-06-23 23:04:49 +0530

    Create BinaryTreeInorderTraversal.cpp

---
+++ Week_4/Day_2/BinaryTreeInorderTraversal.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> res;
+
+        if(root == NULL){
+            return res;
+        }
+
+        vector<int> left = inorderTraversal(root->left);
+        res.insert(res.end(), left.begin(), left.end());
+
+        res.push_back(root->val);
+
+        vector<int> right = inorderTraversal(root->right);
+        res.insert(res.end(), right.begin(), right.end());
+
+        return res;
+
+    }
+};
